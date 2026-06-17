03af453e43149099a0bcd0688747396444cfa3c9
slodzone <polina.morina@icloud.com>
2026-06-17 13:06:48 +0200

    Delete pawmatch directory

--- pawmatch/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import PawMatch from "./PawMatch.jsx";
-
-createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <PawMatch />
-  </React.StrictMode>
-);
